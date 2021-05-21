12fc3a5392ff338301ad8a645cbf66a52d19bbc7
Ottis1 <otto.kuusela@outlook.com>
2021-05-21 17:55:26 +0300

    Add basic json file write functionality

--- app.R
+++ app.R
@@ -1,13 +1,22 @@
 ## R Shiny App for Management Data Input 
 ## Field Observatory
+# Otto Kuusela 2021
 
 library(shiny)
+library(jsonlite)
+
+# make helper functions available
+source("json_file_helpers.R")
 
 # read the csv file containing the sites 
 sites <- read.csv(file = 'data/FOsites.csv')$site
 
 # what are the possible activities that can be submitted?
-possible_activities <- c("planting", "fertilizer", "irrigation", "tillage", "organic_material", "harvest", "bed_prep", "inorg_mulch", "Inorg_mul_rem", "chemicals", "mowing", "observation", "weeding", "puddling", "flood_level", "other")
+possible_activities <- c("planting", "fertilizer", "irrigation", "tillage", 
+                         "organic_material", "harvest", "bed_prep", 
+                         "inorg_mulch", "Inorg_mul_rem", "chemicals", "mowing",
+                         "observation", "weeding", "puddling", "flood_level", 
+                         "other")
 
 # Define UI for the application
 ui <- fluidPage(
@@ -22,20 +31,28 @@ ui <- fluidPage(
         sidebarPanel(
             
             # adding "" to the choices makes the default choice empty
-            selectInput("site", label = "Select the site:", choices = c("", sites)),
+            selectInput("site", label = "Select the site:", 
+                        choices = c("", sites)),
             
-            selectInput("block", label = "Select the block:", choice = c("", "0", "1")),
+            selectInput("block", label = "Select the block:", 
+                        choice = c("", "0", "1")),
             
-            selectInput("activity", label = "Select the activity:", choices = c("", possible_activities)),
+            selectInput("activity", label = "Select the activity:", 
+                        choices = c("", possible_activities)),
             
             dateInput("date", label = "Select the date when the activity was performed:"),
             
-            textAreaInput("notes", label = "Notes (optional):", placeholder = "Anything related to the event, e.g. yield amount, seeding or tillage depth, products spread, machine type, etc."),
+            textAreaInput("notes", label = "Notes (optional):", 
+                          placeholder = paste("Anything related to the event,",
+                          "e.g. yield amount, seeding or tillage depth,", 
+                          "products spread, machine type, etc.", sep = ""), 
+                          resize = "vertical"),
             
-            submitButton(text = "Save")
+            actionButton("submit", label = "Save")
         ),
 
-        # the main panel will in the future contain the options to input more precise data
+        # the main panel will in the future enable the input of more precise data
+        # as well as seeing previous submissions
         mainPanel(
            
         ),
@@ -43,9 +60,21 @@ ui <- fluidPage(
 )
 
 
-# Define server logic TODO
+# Define server logic incl. save button action
 server <- function(input, output) {
-
+    
+    # save input to a file
+    observeEvent(input$submit, {
+        
+        # TODO: fix weird date picker problem ("18977")
+        
+        append_to_json_file(input$site, input$block, input$date,
+                            input$activity, input$notes)
+        
+        # TODO: clear some of the fields?
+        
+        showNotification("Data saved!", type = "message")
+    })
     
 }
 
---
+++ json_file_helpers.R
@@ -0,0 +1,49 @@
+# Helper functions for the management data input app
+# Otto Kuusela 2021
+
+# TODO: a lot of error handling
+
+# missing value in the ICASA standard
+missingval <- "-99.0"
+# relative path to json file folder
+json_file_folder <- "data/management_events/"
+
+create_json_file <- function(file_path) {
+  
+  # create structure with no events
+  experiment <- list()
+  experiment$management <- list()
+  experiment$management$events <- list()
+  
+  # create file
+  jsonlite::write_json(experiment, path = file_path, pretty = TRUE, 
+                       null = 'null', auto_unbox = TRUE)
+  
+}
+  
+append_to_json_file <- function(site_name, block, date, activity, notes) {
+  
+  # corresponding file name
+  file_name <- paste(site_name, block, "events.json", sep = "_")
+  file_path <- paste0(json_file_folder, file_name)
+  
+  
+  # if file doesn't exist, create it
+  if (!file.exists(file_path)) {
+    create_json_file(file_path)
+  }
+  
+  experiment <- jsonlite::fromJSON(file_path)
+  
+  # create a new “row” to be added to experiment$management$events
+  new_event <- data.frame(mgmt_operations_event = activity,
+                          mgmt_event_date = date,
+                          mgmt_event_notes = notes)
+  
+  # add new event
+  experiment$management$events <- rbind(experiment$management$events, new_event)
+  
+  # save changes
+  jsonlite::write_json(experiment, path = file_path, pretty = TRUE, 
+                       null = 'null', auto_unbox = TRUE)
+}
