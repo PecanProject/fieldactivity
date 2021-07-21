517d713a21430b8d3c2341dd4b5d6391e36688e4
Ottis1 <otto.kuusela@outlook.com>
2021-07-21 14:33:42 +0300

    Calculate harvest  yield totals automagically

--- table.R
+++ table.R
@@ -153,6 +153,33 @@ tableServer <- function(id, row_variable_value, language, visible,
             override_trigger(override_trigger() + 1)
         })
         
+        # this is a flag which, when set to TRUE, blocks the calculation of 
+        # sums on the total row. This is used to prevent the calculation when
+        # the widgets are first created and get their initial values in the
+        # table_data reactive.
+        block_sum_calculation <- reactiveVal(FALSE)
+        
+        calculate_sum <- function(total_variable) {
+            if (!static_mode) {
+                row_value <- isolate(table_values())[[row_variable]]
+                row_numbers <- if (is.numeric(row_value)) {
+                    1:row_value
+                } else {
+                    1: length(row_value)
+                }
+            }
+
+            variable_to_sum <- structure_lookup_list[[total_variable]]$sum_of
+            values <- NULL
+            for (row_number in row_numbers) {
+                element_name <- paste(variable_to_sum, row_number, sep = "_")
+                values <- c(values, isolate(input[[element_name]]))
+            }
+            
+            value <- sum(values[!is.na(values)])
+            update_ui_element(session, total_variable, value = value)
+        }
+        
         # this allows blocking extra updates caused by the widget in the main
         # app changing its value after override_values have just been supplied.
         observeEvent(row_variable_value(), ignoreNULL = FALSE, {
@@ -323,8 +350,21 @@ tableServer <- function(id, row_variable_value, language, visible,
                             
                             # the code names for these elements are
                             # variablename_rownumber
-                            code_name <- NS(id, 
-                                            paste(variable, current_row, sep = "_"))
+                            code_name <- paste(variable, current_row, sep = "_")
+                            
+                            # add observer to sum values if requested
+                            # if (!is.null(element$sum_to)) {
+                            #     message(glue("Adding observer for {code_name}, {element$sum_to}"))
+                            #     lapply(list(list(widget = code_name, 
+                            #                 total_variable = element$sum_to)), 
+                            #            FUN = function(x) {
+                            #         observeEvent(input[[x$widget]], su, {
+                            #             message(glue("Observe for {x$widget},",
+                            #                 "{x$total_variable}"))
+                            #             calculate_sum(x$total_variable)
+                            #     })})
+                            # }
+                            
                             # value to show in the widget initially
                             value <- if (do_override) {
                                 override_vals[[variable]][current_row]
@@ -359,7 +399,8 @@ tableServer <- function(id, row_variable_value, language, visible,
                             widget <- as.character(
                                 create_element(element,
                                                #width = width,
-                                               override_code_name = code_name,
+                                               override_code_name = 
+                                                   NS(id, code_name),
                                                override_label = "",
                                                override_value = value,
                                                override_choices = choices,
@@ -402,6 +443,8 @@ tableServer <- function(id, row_variable_value, language, visible,
             #     })
             # })
 
+            # block calculation of sums once when the table becomes visible
+            block_sum_calculation(TRUE)
             # clear override_values
             override_values(NULL)
             if (log) message(glue("Calculated table, has ",
@@ -541,12 +584,13 @@ tableServer <- function(id, row_variable_value, language, visible,
             override_trigger() 
             
             value_list <- list()
+            did_override <- !is.null(isolate(override_values()))
             if (!static_mode) {
                 value_list[[row_variable]] <- 
-                    if (is.null(isolate(override_values()))) {
-                        isolate(row_variable_value())
-                    } else {
+                    if (did_override) {
                         isolate(override_values()[[row_variable]])
+                    } else {
+                        isolate(row_variable_value())
                     }
             }
             
@@ -590,9 +634,20 @@ tableServer <- function(id, row_variable_value, language, visible,
                     element_name <- paste(variable, row_number, sep = "_")
                     values <- c(values, input[[element_name]])
                 }
+                
+                previous_vals <- isolate(table_values())[[variable]]
+                total_variable <- structure_lookup_list[[variable]]$sum_to
+                if (!identical(previous_vals, values) && 
+                    !is.null(total_variable) &&
+                    !isolate(block_sum_calculation())) {
+                    message(glue("Initiating sum calculation for {total_variable}"))
+                    calculate_sum(total_variable)
+                }
+                
                 value_list[[variable]] <- values
             }
-
+            
+            block_sum_calculation(FALSE)
             old_values(value_list)
             table_values(value_list)
         })
